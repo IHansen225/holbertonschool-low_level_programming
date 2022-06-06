d75fed0b197604a1c9acff958f2eeaaffb05fc77
root <ihk225@gmail.com>
2022-06-06 16:37:15 -0700

    C exercises from

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = (n % 10)
+	printf("Last digit of %d is %d ", n, m)
+
+	return (0);
+}
