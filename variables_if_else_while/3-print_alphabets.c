ebab4d9f05352b2dc6d36bb75f18d31802d8b717
root <ihk225@gmail.com>
2022-06-06 18:09:50 -0700

    C exercises from

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha)
+	}
+	putchar(10);
+
+	return (0);
+}
