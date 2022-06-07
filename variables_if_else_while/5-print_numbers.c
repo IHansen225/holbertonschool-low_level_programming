612630c91f6ef310bdbcd4547e84b70a70662b88
root <ihk225@gmail.com>
2022-06-06 18:32:53 -0700

    C exercises from

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 30; alpha <= 39; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar(10);
+
+	return (0);
+}
