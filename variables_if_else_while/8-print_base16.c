4195f4738b94b85a3b16e4e5413f98f8616689bf
root <ihk225@gmail.com>
2022-06-06 18:41:35 -0700

    C exercises from

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = '0'; alpha <= '9'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar(10);
+
+	return (0);
+}
