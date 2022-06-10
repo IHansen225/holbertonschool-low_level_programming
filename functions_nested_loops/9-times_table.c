ef515d04474b8373ae04b4f58574f48a6b6e1b31
root <ihk225@gmail.com>
2022-06-10 07:23:03 -0700

    C exercises from

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,21 +8,21 @@
 
 void times_table(void)
 {
-	int len = 10;
-	int col[len] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int lar = 10;
+	int times[lar] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	for (i = 0; i <= len; i++);
+	for (i = 0; i <= lar; i++);
 	{
 		if (i != 9)
 		{
-			_putchar((col[i] * i) + '0');
+			_putchar((times[i] * i) + '0');
 			_putchar(44);
 			_putchar(32);
 			_putchar(32);
 		}
 		else
 		{
-			_putchar((col[i] * i) + '0');
+			_putchar((times[i] * i) + '0');
 		}
 	}
 }
