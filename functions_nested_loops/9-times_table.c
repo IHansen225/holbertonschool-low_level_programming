02c95937b93d3eb775452b883d36ecc9c6c5636a
root <ihk225@gmail.com>
2022-06-10 10:39:09 -0700

    C exercises from

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,43 +8,23 @@
 
 void times_table(void)
 {
-	int cont, mult, num;
+	int num, mult;
 
-	for (cont = 0; cont <= 9; cont++);
+	for (num = 0; num <= 9; num++)
 	{
 		for (mult = 0; mult <= 9; mult++)
 		{
-			if (mult < 9)
+			if (mult != 9)
 			{
-				if ((cont * mult) > 10)
-				{
-					_putchar(((cont * mult) / 10) + '0');
-					_putchar(((cont * mult) % 10) + '0');
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-				else
-				{
-					_putchar(((cont * mult)) + '0');
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
+				_putchar(mult + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
 			}
 			else
 			{
-				if ((cont * mult) > 10)
-				{
-					_putchar(((cont * mult) / 10) + '0');
-					_putchar(((cont * mult) % 10) + '0');
-					_putchar(10);
-				}
-				else
-				{
-					_putchar(((cont * mult)) + '0')
-					_putchar(10);
-				}
+				_putchar(mult + '0');
+				_putchar(10);
 			}
 		}
 	}
