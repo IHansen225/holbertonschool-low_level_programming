e076e6e85b8b4d22287b2002867d62fe24a4d68f
root <ihk225@gmail.com>
2022-06-10 18:43:44 -0700

    C exercises from

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints sucession to 98
+ * @n: numeric input
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n; n > 98; n--)
+		{
+			printf("%d, ", n);
+		};
+		printf("98");
+	}
+	else if (n == 98)
+	{
+		printf("98");
+	}
+	else
+	{
+		for (n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		};
+		printf("98")
+	}
+}
