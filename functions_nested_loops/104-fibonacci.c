4cabb30819bd80f3ac85198d3c204c50c2354310
root <ihk225@gmail.com>
2022-06-12 12:49:36 -0700

    C exercises from

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,21 +2,23 @@
 #include "main.h"
 
 /**
-* _isupper - checks for uppercase char
-* Return: 1 if true
+* main - prints fibonacci sequence
+* Return: o
 */
 
-void main(void)
+int main(void)
 {
 	int nprint, n1, n2, cont;
 
 	n1 = 0;
 	n2 = 1;
-	for (cont = 0; cont <= 98; n++)
+	for (cont = 0; cont <= 98; cont++)
 	{
 		nprint = n1 + n2;
 		printf("%d, ", nprint);
 		n1 = n2;
 		n2 = nprint;
 	}
+
+	return (0);
 }
