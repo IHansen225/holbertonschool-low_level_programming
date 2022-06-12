210f059a1b88acce4561d66cedb51e71ca9ac3a2
root <ihk225@gmail.com>
2022-06-12 12:09:09 -0700

    C exercises from

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - multiplies numbers
+* @a: input 1
+* @b: input 2
+* @c: result
+* Return: multiplied numbers
+*/
+
+int mul(int a, int b)
+{
+	int c = (a * b);
+
+	return (c);
+}
