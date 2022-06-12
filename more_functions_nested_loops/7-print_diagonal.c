21cfa672a2abb2debf352d260915473fa228080e
root <ihk225@gmail.com>
2022-06-12 14:41:33 -0700

    C exercises from

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_diagonal - prints a line
+* @n: line length
+* Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i, j, spac;
+	spac = 0;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n, i++)
+		{
+			for (j = 0; j < spac; j++)
+				_putchar(32);
+			_putchar(92);
+			_putchar(10);
+		}
+		spac++;
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
