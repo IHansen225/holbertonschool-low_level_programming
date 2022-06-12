2ba658b411204a3d1f23641acc66c8a7930ac2f8
root <ihk225@gmail.com>
2022-06-12 12:58:34 -0700

    C exercises from

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,17 +9,25 @@
 int main(void)
 {
 	unsigned long nprint, n1, n2, cont;
-	
 
 	n1 = 0;
 	n2 = 1;
 	for (cont = 0; cont <= 98; cont++)
 	{
-		nprint = n1 + n2;
-		printf("%lu, ", nprint);
-		n1 = n2;
-		n2 = nprint;
+		if !(cont == 98)
+		{
+			nprint = n1 + n2;
+			printf("%lu, ", nprint);
+			n1 = n2;
+			n2 = nprint;
+		}
+		else
+		{
+			nprint = n1 + n2;
+			printf("%lu", nprint);
+		}
 	}
+	printf("\n");
 
 	return (0);
 }
