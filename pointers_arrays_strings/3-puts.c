0ebd7a6eba12f0165ba6a03f59ac8ed03a34afcc
root <ihk225@gmail.com>
2022-06-14 10:25:02 -0700

    C exercises from

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - counts characters on string 
+ * _puts - counts characters on string
  * @str: pointer
  * Return: nothing
  */
@@ -9,7 +9,7 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
