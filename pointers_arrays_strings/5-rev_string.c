fd9b1dadd93a324db99d4bca78a2443e5349811e
root <ihk225@gmail.com>
2022-06-14 11:08:01 -0700

    C exercises from

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,22 @@
  */
 void rev_string(char *s)
 {
-	int i = _strlen(s);
-	char mirror[i];
+	int len = _strlen(s);
+	char mirror[len];
+	int i, j;
+
+	i = 0;
+	j = len;
+	while (s[i] != '\0')
+	{
+		mirror[j] = s[i]
+		i++;
+		j--;
+	}
+	i = 0;
+	while (i >= len)
+	{
+		*s[i] = mirror[i];
+		i++;
+	}
 }
