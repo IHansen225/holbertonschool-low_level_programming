87e6add1973abb8354286c7add33ace2c38e2d6d
root <ihk225@gmail.com>
2022-06-14 16:21:44 -0700

    C exercises from

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,7 +3,7 @@
 /**
  * rev_string - reverses a string
  * @s: string input
- * 
+ *
  * Returns: nothing
  */
 void rev_string(char *s)
@@ -17,9 +17,10 @@ void rev_string(char *s)
 		len++;
 	}
 
-	for (i = 0; i < (len/2); i++){
+	for (i = 0; i < (len / 2); i++)
+	{
 		temp = s[i];
-		s[i] = s[len-i-1];
-		s[len-i-1] = temp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
