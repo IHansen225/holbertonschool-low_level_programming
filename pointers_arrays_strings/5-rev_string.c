c19a0208a5eee1447617918ba4ec9eb7f3ef54da
root <ihk225@gmail.com>
2022-06-14 16:19:49 -0700

    C exercises from

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,18 @@
  */
 void rev_string(char *s)
 {
-    int len = _strlen(s);
-    int i;
-    char temp;
+	int len = 0;
+	int i;
+	char temp;
 
-    for(i = 0; i<len/2; i++){
-        temp = s[i];
-        s[i] = s[len-i-1];
-        s[len-i-1] = temp;
-    }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < (len/2); i++){
+		temp = arr[i];
+		arr[i] = arr[len-i-1];
+		arr[len-i-1] = temp;
+	}
 }
