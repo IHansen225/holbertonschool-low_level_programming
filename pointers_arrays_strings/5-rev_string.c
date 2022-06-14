9739ac987299a6cf055056bb08fc03183afb0c0e
root <ihk225@gmail.com>
2022-06-14 15:47:04 -0700

    C exercises from

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,25 @@
 #include "main.h"
 
 /**
- * rev_string - counts characters on string
- * @s: pointer
- * Return: nothing
+ * rev_string - reverses a string
+ * @s: string input
+ * 
+ * Returns: nothing
  */
+
 void rev_string(char *s)
 {
-	int len = _strlen(s);
-	int i, j;
-	char mirror[len];
+    int len = _strlen(s)
+    char *first = str;
+    char *last = start + len - 1;
+    char aux;
+
+    while (last > first)
+    {
+        aux = *first;
+        *first = *last;
+        *last = aux;
+        ++first;
+        --last;
+    }
 }
