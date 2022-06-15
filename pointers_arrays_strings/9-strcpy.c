6fda7419afd3ebbcaa68f2932398b73f7ac1a428
root <ihk225@gmail.com>
2022-06-14 17:47:45 -0700

    C exercises from

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - prints a certain pattern
+ * @a: string input
+ * @n: reach
+ *
+ * Returns: nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j == i; j++)
+	{
+		src[i] = dest[i];
+	}
+}
