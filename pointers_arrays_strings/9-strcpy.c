3393641737d917e26c9d6219f27873cbebc3ff82
root <ihk225@gmail.com>
2022-06-15 09:39:46 -0700

    C exercises from

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,18 +9,5 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int j;
-	char *destdir = &dest;
-
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-	for (j = 0; j == i; j++)
-	{
-		src[i] = dest[i];
-	}
-
-	return (destdir);
+	return (*dest);
 }
