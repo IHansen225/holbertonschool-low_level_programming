ab7d958d923929d5a01e962d95cfecab62d269d1
root <ihk225@gmail.com>
2022-06-15 10:05:35 -0700

    C exercises from

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_array - prints a certain pattern
- * @a: string input
- * @n: reach
+ * _strcpy - copies a ecrtain array
+ * @dest: string input
+ * @src: reach
  *
- * Returns: nothing
+ * Returns: pointer
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -17,6 +17,7 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i + 1] = '\0';
 
 	return (dest);
 }
