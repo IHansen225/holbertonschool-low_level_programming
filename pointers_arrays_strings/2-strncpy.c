b76af94d8bc303508cd1da95b14e80b8b6ad59d2
nanxuss <ihk225@gmail.com>
2022-06-16 15:50:08 -0700

    C exercises from

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,18 +2,20 @@
 
 /**
  * _strncpy - copies a string
- * 
+ * @dest: pointer
+ * @src: pointer
+ * @n: byte size;
  * 
  * Returns: pointer to dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-    for ( ; i < n; i++)
+	for ( ; i < n; i++)
 		dest[i] = '\0';
 	
 	return (dest);
