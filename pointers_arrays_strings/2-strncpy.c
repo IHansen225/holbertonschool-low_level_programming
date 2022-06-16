ed1c3c43c01665bd76de67ffdbabb4c1f71a4e2a
nanxuss <ihk225@gmail.com>
2022-06-16 15:10:51 -0700

    C exercises from

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * 
+ * 
+ * Returns: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = (sizeof(dest) / sizeof(dest[0]));
+	j = 0;
+	
+	return (dest);
+}
