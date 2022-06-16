7b19d057ed8de61d702c655f7f14ddb784ff5a32
root <ihk225@gmail.com>
2022-06-15 19:36:38 -0700

    C exercises from

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - changes a certain value
+ * @dest: pointer
+ * @src: pointer
+ * @n: byte length
+ * Return: pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0, j <= n, j++)
+	{
+		dest[i] = src[j];
+		i++
+	}
+	dest[i] = '\0'
+
+	return (dest);
+}
