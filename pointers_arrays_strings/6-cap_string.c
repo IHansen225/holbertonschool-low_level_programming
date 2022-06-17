8c3ddcf02f973cbbb3a5d91336e8b8fd1e771853
nanxuss <ihk225@gmail.com>
2022-06-17 10:06:29 -0700

    C exercises from

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * cap_string: capitalizes a string
+ * @a: string 
+ * 
+ * return: nothing
+ */
+
+char *cap_string(char *a)
+{
+	char sep[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+	int i, j;
+	int flag;
+
+	for (i = 0; a[i] == '\0'; i++)
+    {
+		if (flag = 0);
+		{
+			for (j = 0; j <= 13; j++)
+			{
+					if (a[i] == sep[j])
+					{
+						flag = 1;
+						break;
+					}
+					else
+					{
+						continue;
+					}
+				}
+		}
+		else
+		{
+			if ((a[i] > 96) && (a[i] < 123))
+			{
+				a[i] = a[i] - 32;
+				flag = 0;
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+}
