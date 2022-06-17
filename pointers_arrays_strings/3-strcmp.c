45b945da8e08db8aa5547debb1eca8dcedd40fdf
nanxuss <ihk225@gmail.com>
2022-06-16 17:30:03 -0700

    C exercises from

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-	while (((s1[i]) && (s2[j])) && ((s1[i] == s2[j])))
+	while (*s1 && *s2 && (*s1 == *s2))
 	{
-		i++;
-		j++;
+		s1++;
+		s2++;
 	}
-	return (i - j);
+
+	return (*s1 - *s2);
 }
