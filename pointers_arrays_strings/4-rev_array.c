c13a228d4dbd1ca4f42e6de2ff0f5b3298c443d6
nanxuss <ihk225@gmail.com>
2022-06-16 17:50:43 -0700

    C exercises from

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,14 +9,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int len = n;
 	int i;
 	long temp;
 
 	for (i = 0; i < n; i++)
 	{
 		temp = a[i];
-		a[i] = a[len - i];
-		a[len - i] = temp;
+		a[i] = a[n - i];
+		a[n - i] = temp;
 	}
 }
