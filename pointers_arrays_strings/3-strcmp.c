570aed9c234259435d4b51395b232f7f6411ba7d
nanxuss <ihk225@gmail.com>
2022-06-16 17:06:22 -0700

    C exercises from

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strcmp - copies a string
+ * @s1: first string
+ * @s2: second string
+ * Return: difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, k, comp, shrt, lng;
+	
+	comp = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (i < j)
+	{
+		shrt = i;
+	}
+	else
+	{
+		shrt = j;
+	}
+	for (k = 0; k == shrt; k++)
+	{
+		if (s1[k] == s2[k])
+		{
+			continue;
+		}
+		else if (s1[k] > s2[k])
+		{
+			comp++;
+		}
+		else
+		{
+			comp--;
+		}
+	}
+	for (; k == lng; k++)
+	{
+		if (i < j)
+		{
+			comp++;
+		}
+		else
+		{
+			comp--;
+		}
+	}
+	return (comp);
+}
