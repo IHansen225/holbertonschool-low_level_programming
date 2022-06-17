04c681a39dd00730049304f1f4eba249021c793e
nanxuss <ihk225@gmail.com>
2022-06-16 18:21:54 -0700

    C exercises from

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - copies a string
+ * 
+ * Return: difference
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+	while (*[i] != '\0')
+	{
+		i++;
+	}
+}
