78edac90e4f4d0cc2188d1960857497c2994a117
nanxuss <ihk225@gmail.com>
2022-06-18 13:47:02 -0700

    C exercises from

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -19,14 +19,14 @@ char *_strchr(char *s, char c)
 	{
 		i++;
 	}
-	for (j = 0; j < i; j++)
+	for (j = 0; j <= i; j++)
 	{
 		if (s[j] == c)
 		{
 			break;
 		}
 	}
-	if (j == i)
+	if (j > i)
 	{
 		p = NULL;
 		return (p);
