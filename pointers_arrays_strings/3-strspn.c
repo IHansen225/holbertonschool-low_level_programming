95a716913e0796112b529c975ad15e784e3dc1be
nanxuss <ihk225@gmail.com>
2022-06-18 21:12:48 -0700

    C exercises from

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -4,21 +4,36 @@
  * _strspn - returns length of a certain pattern
  * @s: string
  * @accept: characters to find
- *
+ * 
  * Return: length of a given pattern
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, flag;
 	unsigned int count = 0;
 
+	flag = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
-		if (s[i] == accept[j])
+		if (flag == 0)
 		{
-			count++;
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+				{
+					count++;
+					break;
+				}
+				else if (accept[j + 1] == '\0')
+				{
+					flag = 1;
+					break;
+				}
+			}
+		}
+		else
+		{
 			break;
 		}
 	}
