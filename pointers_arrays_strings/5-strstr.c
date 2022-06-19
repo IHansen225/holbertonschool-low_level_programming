264fc8fd0adf3e964225d98db3d1816154fc1f7d
nanxuss <ihk225@gmail.com>
2022-06-19 12:55:05 -0700

    C exercises from

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - returns length of a certain pattern
+ * @haystack: string
+ * @needle: characters to find
+ * 
+ * Return: length of a given pattern
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, temp;
+	char *s;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			s = &haystack[i];
+			temp = i + 1;
+			for (j = 1; haystack[j]; j++)
+			{
+				if (haystack[j] == needle[j])
+				{
+					if (needle[j + 1] == '\0')
+					{
+						return (s);
+					}
+					else
+					{
+						continue;
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+	s = NULL;
+	return (s);
+}
