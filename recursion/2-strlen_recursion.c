12ebc5ab26155e2c7160832c97fcae7b65d0c93c
nanxuss <ihk225@gmail.com>
2022-06-20 11:22:29 -0700

    C exercises from

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - first recursion function
+ * @s: string
+ * 
+ * Return: nothing
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		return (s);
+	}
+	else
+	{
+		_strlen_recursion(s + 1);
+	}
+}
