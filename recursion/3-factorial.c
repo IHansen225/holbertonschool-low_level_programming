af9e3dfdbf3aa052d49523e8c98929c39e99a73c
nanxuss <ihk225@gmail.com>
2022-06-20 12:37:11 -0700

    C exercises from

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - first recursion function
+ * @s: string
+ * 
+ * Return: nothing
+ */
+
+int factorial(int n);
+{
+	if (n > 1)
+		return (n * factorial(n - 1));
+	if (n == 1 || n == 0)
+		return (1);
+	if (n < 1)
+		return (-1);
+}
