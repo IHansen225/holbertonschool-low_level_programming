6c633084f0201e893f89121677ff7485aac9b523
nanxuss <ihk225@gmail.com>
2022-06-20 11:55:39 -0700

    C exercises from

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,12 +9,9 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (s == '\0')
 	{
-		return (s);
+		return (0);
 	}
-	else
-	{
-		_strlen_recursion(s + 1);
-	}
+	return (1 + _strlen_recursion(s + 1));
 }
