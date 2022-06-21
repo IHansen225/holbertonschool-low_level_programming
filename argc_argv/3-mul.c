3f3cee500ed264779e62d7b82d1f5020ea337c52
nanxuss <ihk225@gmail.com>
2022-06-21 14:55:06 -0700

    C exercises from

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints multiplication
+ * @argc: number of arguments
+ * @argv: arguments
+ * 
+ * Return: nothing
+ */
+
+int main(int argc, char *argv)
+{
+	int mult;
+
+	if (argc < 3)
+	{
+		printf("Error\n")
+	}
+	else
+	{
+		mult = argv[1] * argv[2]
+		return (mult);
+	}
+
+}
