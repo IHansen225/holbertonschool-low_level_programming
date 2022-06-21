8609984e678d7debc490b30ce96a6e4d7813615b
nanxuss <ihk225@gmail.com>
2022-06-21 13:41:11 -0700

    C exercises from

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: argument array
+ *
+ * Return: nothing
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
