d24b3367a461adf42dad1b4a66edb7dd83ebe837
nanxuss <ihk225@gmail.com>
2022-06-21 13:36:49 -0700

    C exercises from

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints program name
- * 
+ *
  * Return: nothing
  */
 
 int main(int argc, char *argv[])
 {
-    printf("%s\n", argv[argc - argc]);
+	printf("%s\n", argv[argc - argc]);
 
-    return (0);
+	return (0);
 }
