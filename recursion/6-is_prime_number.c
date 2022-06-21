08daaa22b57a2292c90d0691f60a41e517916684
nanxuss <ihk225@gmail.com>
2022-06-20 20:21:30 -0700

    C exercises from

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,9 +10,13 @@
 
 int _recusrive_division(int x, int y)
 {
-	if ((x % y != 0) && (x != y))
+	if ((x % y != 0) && (x != y) && (x != 1))
 	{
 		return (_recusrive_division(x, (y + 1)));
+	}
+	else if ((x == 1) || (x < 0))
+	{
+		return (0);
 	}
 	else
 	{
