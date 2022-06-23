7ec5f127b248f0d8aa0cf81e27a1440ae7f6a6b2
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-23 01:02:31 -0300

    C exercises from

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,22 +10,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr[size];
 	char *ret;
-	int i;
 
-	if (size == 0)
+	if (size < 1)
 	{
 		ret = NULL;
 	}
 	else
 	{
-		for (i = 0; arr[i]; i++)
-		{
-			arr[i] = c;
-		}
-		ret = arr;
+		malloc((sizeof(int)) * size);
 	}
-
-	return (ret);
 }
