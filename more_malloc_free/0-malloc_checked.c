8115118b2e5e718a7002b86c3bbb84e0c76d310c
nanxuss <ihk225@gmail.com>
2022-06-24 06:47:53 -0700

    C exercises from

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - prints multiplication
+ * @b: unsigned int
+ *
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ret = malloc(b);
+	if (ret == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (ret);
+	}
+}
