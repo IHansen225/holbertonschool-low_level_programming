862d3d16e7ba98ef588efff05c4182c562861cb6
nanxuss <ihk225@gmail.com>
2022-06-24 07:28:33 -0700

    C exercises from

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i <= len1; i++)
 		s3[i] = s1[i];
 	for (j = 0; j <= len2; j++)
+	{
 		s3[i] = s2[j];
 		i++;
+	}
 	return (s3);
 }
