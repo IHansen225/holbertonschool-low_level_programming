8c056e0a0421860bed456bbd1f32b6b60688102a
nanxuss <ihk225@gmail.com>
2022-06-24 10:23:39 -0700

    C exercises from

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	arr = malloc(len * (sizeof(int)));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)
+	for (i = 0; i < len; i++)
 		arr[i] = min + i;
 	return (arr);
 }
