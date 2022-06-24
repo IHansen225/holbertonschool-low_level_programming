0c273ef1f4008dbe98fb4b4556087b0814c74e85
nanxuss <ihk225@gmail.com>
2022-06-24 08:27:30 -0700

    C exercises from

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - concats a string with another
+ * @nmemb: int
+ * @size: unsigned int
+ *
+ * Return: nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	void *res;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	res = malloc(size * nmemb);
+	if (res == NULL)
+		return (NULL);
+	
+	return (res);
+}
