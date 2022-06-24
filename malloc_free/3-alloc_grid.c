1f46cd88088209d9fbfb1aced631fdcbf1abd2ec
nanxuss <ihk225@gmail.com>
2022-06-24 05:31:15 -0700

    C exercises from

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,12 +13,28 @@
 int **alloc_grid(int width, int height)
 {
 	int i;
+	int **mat;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
-	for (i = 0; i < width; i++)
-		mat[i] = (int*)malloc(height * sizeof(int));
-
-	return (mat);
+	mat = malloc(height * sizeof(int *));
+	if (mat == NULL)
+		return (NULL);
+	for (int i = 0; i < width; i++)
+	{
+		mat[i] = malloc(width * sizeof(int));
+		if (mat[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(mat[i]);
+			free(mat);
+			return (NULL);
+		}
+		else
+		{
+			for (j = 0; mat[i][j]; j++)
+				mat[i][j] = 0;
+		}
+		return (mat);
+	}
 }
