eb20fb10f895407f822f0b5e6eba6f24d16ed38c
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-27 22:14:16 -0300

    C exercises from

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,18 +11,16 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
 	if (d->name == NULL)
-		return;
+		printf("(nil)");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == 0)
-		return;
+		printf("(nil)");
 	else
 		printf("Age: %.1f\n", d->age);
 	if (d->owner == NULL)
-		return;
+		printf("(nil)");
 	else
 		printf("Owner: %.s\n", d->owner);
 }
