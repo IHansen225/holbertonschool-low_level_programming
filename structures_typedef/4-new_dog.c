f16ed25d82c8ae19871be2f8485b43717c37080b
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-27 22:50:47 -0300

    C exercises

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - creates a new_dog
+ * @name: name of the new_dog
+ * @age: age of the new_dog
+ * @owner: owner of the new_dog
+ *
+ * Return: nothing
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
