ec53bba43e8444949443d92e278264c3901f598e
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-28 17:58:11 -0300

    C exercises

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,5 +13,24 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	
+    dog_t *newdog;
+    int nlen, olen;
+    int i = 0;
+
+    if (name == NULL || owner == NULL)
+        return (NULL);
+    for (;name[i]; i++)
+        nlen++;
+    for (i = 0; owner[i]; i++)
+        olen++;
+    newdog->name = malloc(nlen + 1);
+    newdog->owner = malloc(olen + 1);
+    newdog->age = age;
+    if (newdog->name == NULL || newdog->owner == NULL || newdog->age == NULL)
+        return (NULL);
+    for (i = 0; name[i]; i++)
+        newdog->name[i] = name[i];
+    for (i = 0; owner[i]; i++)
+        newdog->owner[i] = owner[i];
+    return (newdog);
 }
