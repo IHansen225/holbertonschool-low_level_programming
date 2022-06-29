b2160e73c43ed6d467d7466a66dad60b01c32d40
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-28 21:21:02 -0300

    C exercises

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -45,7 +45,7 @@ dog_t *new_dog(char *name, float age, char *owner)
     for (i = 0; i < olen; i++)
         newdog->owner[i] = owner[i];
     newdog->age = age;
-	bytes = bytes - 24;
+	free(24);
 
     return (newdog);
 }
