d3be1aa15f10bc03ac4598e2363654ee78e9ee07
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-29 01:45:14 -0300

    C exercises

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,24 +16,24 @@ int main(int argc, char *argv[])
 {
 
 	if (argc < 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    if (((*argv[2] == '/') || (*argv[2] == '%')) && (atoi(argv[3]) == 0))
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    if ((*argv[2] == '+') || (*argv[2] == '-') || (*argv[2] == '*') || (*argv[2] == '/') || (*argv[2] == '%'))
-    {
-        (*(get_op_func(argv[2])))(atoi(argv[1]), atoi(argv[3]));
-    }
-    else
-    {
-        printf("Error\n");
-        exit(99);
-    }
-    
-    return (0);
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (((*argv[2] == '/') || (*argv[2] == '%')) && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if ((*argv[2] == '+') || (*argv[2] == '-') || (*argv[2] == '*') || (*argv[2] == '/') || (*argv[2] == '%'))
+	{
+		(*(get_op_func(argv[2])))(atoi(argv[1]), atoi(argv[3]));
+	}
+	else
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	return (0);
 }
