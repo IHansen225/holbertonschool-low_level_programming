6e81a202d55ba7650923179b9c31683d9f96a438
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-29 00:31:53 -0300

    C exercises

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * main - recieves arguments and returns result
+ * @argc: argument count
+ * @argv: argument list
+ *
+ * Return: result of function
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+    if (((argv[2] == '/') || (argv[2] == '%')) && (atoi(argv[4]) == 0))
+    {
+        printf("Error\n");
+        exit(100);
+    }
+    if ((argv[2] == '+') || (argv[2] == '-') || (argv[2] == '*') || (argv[2] == '/') || (argv[2] == '%'))
+    {
+
+    }
+    else
+    {
+        printf("Error\n");
+        exit(99);
+    }
+    
+    return (0);
+}
