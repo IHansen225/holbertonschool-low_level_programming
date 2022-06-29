95bc07e5d8fcadab54ed9ff75b70c8c3aaca16bf
Ivan Hansen <ihansen@pop-os.localdomain>
2022-06-28 22:34:59 -0300

    C exercises

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - print the dog struct
+ * @array: print
+ * @size: size of the array
+ * @action: function to be called
+ *
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	
+	if (!array || !action)
+		return;
+	else
+	{
+		for (i = 0; i < size; i++)
+		action(array[i]);
+	}
+}
