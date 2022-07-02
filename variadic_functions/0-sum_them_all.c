4b182ffecd3f15d1759346feabead5114f6fa037
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-02 16:15:59 -0300

    C exercises

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,9 +9,10 @@
 int sum_them_all(const unsigned int n, ...)
 {
     int res = 0;
+    int i;
     va_list ap;
     va_start(ap, n);
-        for (int i = 0; i < n; i++)
+        for (i = 0; i < n; i++)
         {
             res += va_arg(ap, unsigned int);
         }
