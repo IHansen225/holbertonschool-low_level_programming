48cb2f2ce1070aec6fe7b3c548949f368aa0d276
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-02 16:43:14 -0300

    C exercises

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all arguments
+ *
+ * @n: elements to print
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (separator)
+			{
+				printf("%s", separator);
+			}
+		}
+	va_end(ap);
+	printf("\n");
+}
+
