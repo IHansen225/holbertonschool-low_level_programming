18f4b1c4393859c8963c368f495b2bb9687ff449
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-02 16:46:15 -0300

    C exercises

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(ap, int));
-			if (separator)
+			if ((separator) && ((i + 1) != n))
 			{
 				printf("%s", separator);
 			}
