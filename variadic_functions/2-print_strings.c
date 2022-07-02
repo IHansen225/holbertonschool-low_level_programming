fd3bb5817fadf3d2e789aedef725ce879624470c
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-02 17:22:39 -0300

    C exercises

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -27,7 +27,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", stp);
 			else
 				printf("(nil)");
-			
 			if ((separator) && ((i + 1) != n))
 			{
 				printf("%s", separator);
