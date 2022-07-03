3f3349062a4e27c82d81e0a310f604d938e711c5
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-03 19:29:32 -0300

    C exercises

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,8 +12,10 @@
 
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, i;
+int sum1, sum2, i;
 
+sum1 = 0;
+sum2 = 0;
 	for (i = 0; i < size; i++)
 	{
 		sum1 = sum1 + a[i * size + i];
@@ -22,5 +24,5 @@ void print_diagsums(int *a, int size)
 	{
 		sum2 += a[i * size + (size - i - 1)];
 	}
-	printf("%d, %d\n", sum1, sum2);
+		printf("%d, %d\n", sum1, sum2);
 }
