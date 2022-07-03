f7621d7844c3009c7e32c0055fcae370e5b4cd73
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-03 19:17:44 -0300

    C exercises

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -19,37 +19,37 @@ void print_all(const char * const f, ...)
 	char *stcpy;
 
 	va_start(ap, f);
-        while ((f != NULL) && (f[i] != '\0'))
-        {
-            switch (f[i])
-            {
-                case 'c':
-                    printf("%c", (va_arg(ap, int)));
-                    break;
-                case 'f':
-                    printf("%f", (va_arg(ap, double)));
-                    break;
-                case 's':
-                    stcpy = (va_arg(ap, char *));
-                    if (stcpy == NULL)
-                    {
-                        printf("(nil)");
-                        break;
-                    }
-                    printf("%s", stcpy);
-                    break;
-                case 'i':
-                    printf("%d", (va_arg(ap, int)));
-                    break;
-                default:
-                    i++;
-                    continue;
-            }
-            if (f[i + 1] != '\0')
-                printf(", ");
-            i++;
-        }
+		while ((f != NULL) && (f[i] != '\0'))
+		{
+			switch (f[i])
+			{
+				case 'c':
+					printf("%c", (va_arg(ap, int)));
+					break;
+				case 'f':
+					printf("%f", (va_arg(ap, double)));
+					break;
+				case 's':
+					stcpy = (va_arg(ap, char *));
+					if (stcpy == NULL)
+					{
+						printf("(nil)");
+						break;
+					}
+					printf("%s", stcpy);
+					break;
+				case 'i':
+					printf("%d", (va_arg(ap, int)));
+					break;
+				default:
+					i++;
+					continue;
+			}
+			if (f[i + 1] != '\0')
+				printf(", ");
+			i++;
+		}
 	va_end(ap);
 
-    printf("\n");
+	printf("\n");
 }
