620d0e43290f6e8ec822f9ba7d1e66664eb52ca6
Ivan Hansen <ihk225@gmail.com>
2022-07-13 13:15:37 -0300

    Commit exercises

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- * @brief 
- * 
+ * print_list - print a list
+ *
+ * @h: list header
+ * Return: 0
  */
 
 size_t print_list(const list_t *h)
@@ -12,17 +14,17 @@ size_t print_list(const list_t *h)
 
    for (; p != NULL; p = p->next)
    {
-    if (p->str != NULL)
-    {
-        printf("[%u] %s", p->len, p->str);
-    }
-    else
-    {
-        printf("[0] (nil)");
-    }
-    i++;
-    putchar('\n');
+	if (p->str != NULL)
+	{
+		printf("[%u] %s", p->len, p->str);
+	}
+	else
+	{
+		printf("[0] (nil)");
+	}
+	i++;
+	putchar('\n');
    }
 
-   return (i); 
+   return (i);
 }
