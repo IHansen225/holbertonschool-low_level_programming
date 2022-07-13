c6df9baa3a099f43db16fea225a01b989758ded7
Ivan Hansen <ihk225@gmail.com>
2022-07-13 13:13:12 -0300

    Commit exercises

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+size_t print_list(const list_t *h)
+{
+   list_t *p = h;
+   int i = 0;
+
+   for (; p != NULL; p = p->next)
+   {
+    if (p->str != NULL)
+    {
+        printf("[%u] %s", p->len, p->str);
+    }
+    else
+    {
+        printf("[0] (nil)");
+    }
+    i++;
+    putchar('\n');
+   }
+
+   return (i); 
+}
