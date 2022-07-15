afa0439e7d5b80f5e2edac3c3939815d3e9ea201
Ivan Hansen <ihk225@gmail.com>
2022-07-15 15:26:32 -0300

    Commit 15/7

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (newelement == NULL)
 		return (NULL);
 	newelement->next = *head;
-    newelement->n = n;
+	newelement->n = n;
 	*head = newelement;
 
 	return (*head);
