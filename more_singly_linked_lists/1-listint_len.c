de47df7cacb4ed907de991537956538996cdb615
Ivan Hansen <ihk225@gmail.com>
2022-07-15 15:18:10 -0300

    Commit 15/7

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints list
+ * listint_len - prints list length
  *
  * @h: list header
  * Return: 0
@@ -9,7 +9,7 @@
 
 size_t listint_len(const listint_t *h)
 {
-    const listint_t *p = h;
+	const listint_t *p = h;
 	int i = 0;
 
 	for (; p != NULL; p = p->next)
