6d98d991d4cc9d4334d2b99e62f25385d5fd3910
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-14 21:50:02 -0300

    C exercises

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds node at end of list
+ *
+ * @head: list header
+ * @str: list string
+ * Return: 0
+ */
+
+void free_list(list_t *head)
+{
+	free(head);
+}
