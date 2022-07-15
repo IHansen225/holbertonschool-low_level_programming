325c4afc6f7de27a642a612d9d316744727163f6
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-14 21:13:45 -0300

    C exercises

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - prints list length
+ *
+ * @h: list header
+ * Return: 0
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *newelement;
+    
+    newelement = malloc(sizeof(list_t));
+    if (newelement == NULL)
+        return (NULL);
+    newelement->next = *head;
+    newelement->str = strdup(str);
+    newelement->len = strlen(str);
+    *head = newelement;
+
+    return (*head);
+}
