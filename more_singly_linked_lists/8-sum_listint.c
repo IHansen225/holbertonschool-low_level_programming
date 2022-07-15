662939c24758d1f86e62687c4591b2ca39e75d1b
Ivan Hansen <ihk225@gmail.com>
2022-07-15 20:50:56 -0300

    Commit 15/7

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - finds nth node in the list
+ *
+ * @head: list header
+ * @index: index to find
+ * Return: 0
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *p;
+	int i = 0;
+
+	if (head)
+	{
+		p = head;
+	}
+	else
+	{
+		return (NULL);
+	}
+	while (p->next != NULL)
+	{
+		i += p->n;
+        p = p->next;
+	}
+
+	return (p);
+}
