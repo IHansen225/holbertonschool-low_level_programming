f07ba049cd2970459c82dfb6ec5f4d1f43d6b984
Ivan Hansen <ihk225@gmail.com>
2022-07-15 20:13:31 -0300

    Commit 15/7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,14 +14,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
     unsigned int i = 0;
 
 	p = head;
-	while ((p->next != NULL) && (i <= index))
+	while ((p->next != NULL) && (i != index))
     {
 		p = p->next;
         i++;
-    }
-    if (i > index)
-    {
-        return (NULL);
     }
 
 	return (p);
