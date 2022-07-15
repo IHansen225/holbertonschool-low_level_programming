46ba10ed3c41a91e48df9d72b5ddb850b8983bf4
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-14 21:14:53 -0300

    C exercises

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,15 +9,15 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *newelement;
-    
-    newelement = malloc(sizeof(list_t));
-    if (newelement == NULL)
-        return (NULL);
-    newelement->next = *head;
-    newelement->str = strdup(str);
-    newelement->len = strlen(str);
-    *head = newelement;
+	list_t *newelement;
 
-    return (*head);
+	newelement = malloc(sizeof(list_t));
+	if (newelement == NULL)
+		return (NULL);
+	newelement->next = *head;
+	newelement->str = strdup(str);
+	newelement->len = strlen(str);
+	*head = newelement;
+
+	return (*head);
 }
