e10d5a5621ba150a61910d247cee38eae5579d3a
Ivan Hansen <ihk225@gmail.com>
2022-07-15 16:00:33 -0300

    Commit 15/7

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - adds node at end of list
+ *
+ * @head: list header
+ * Return: 0
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *p_elem, *elem;
+
+	elem = head;
+	while (elem)
+	{
+		p_elem = elem;
+		elem = elem->next;
+		free(p_elem);
+	}
+}
