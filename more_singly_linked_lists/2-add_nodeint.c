5fa6020dce79128b5fe4281c76e17b8dab6d053d
Ivan Hansen <ihk225@gmail.com>
2022-07-15 15:25:50 -0300

    Commit 15/7

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - prints list length
+ *
+ * @head: list header
+ * @n: number to add
+ * Return: 0
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newelement;
+
+	newelement = malloc(sizeof(listint_t));
+	if (newelement == NULL)
+		return (NULL);
+	newelement->next = *head;
+    newelement->n = n;
+	*head = newelement;
+
+	return (*head);
+}
