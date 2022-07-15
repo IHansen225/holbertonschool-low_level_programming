b67ae66e00c764c4935efcb4d7775b61dfaf83f9
Ivan Hansen <ihk225@gmail.com>
2022-07-15 19:25:20 -0300

    Commit exercises

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -15,10 +15,10 @@ int pop_listint(listint_t **head)
 	if (!*head)
 		return (0);
 	else
-		nextelement = **head->next;
-	n = **head->n;
+		nextelement = *head->next;
+	n = *head->n;
 	free(*head);
 	*head = nextelement;
 
-	return (*head);
+	return (n);
 }
