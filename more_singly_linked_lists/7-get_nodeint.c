ead0e6f3acd13a49c7e633e2e0d526690d99a7c4
Ivan Hansen <ihk225@gmail.com>
2022-07-15 20:10:56 -0300

    Commit 15/7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - finds nth node in the list
+ *
+ * @head: list header
+ * @index: index to find
+ * Return: 0
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *newelement, *p;
+    int i = 0;
+
+	p = head;
+	while ((p->next != NULL) && (i <= index))
+    {
+		p = p->next;
+        i++;
+    }
+    if (i > index)
+        return (NULL);
+
+	return (p);
+}
