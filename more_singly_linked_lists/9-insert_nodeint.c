91657f0b27e7e54ee13bb85ada07a591817409d0
Ivan Hansen <ihk225@gmail.com>
2022-07-15 22:23:55 -0300

    Commit 15/7

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newelement->n = n;
     prev = *head;
-    while (prev) && (i != (idx - 1));
+    while ((prev) && (i != (idx - 1)));
     {
         prev = prev->next;
     }
