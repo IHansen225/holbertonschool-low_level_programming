6ff22eb690b3aa129e8bb906d08fef92142fbbe2
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-19 20:20:01 -0300

    C exercises

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,21 +9,21 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i, ret;
-	int f_dig = 0;
+	unsigned long int ret;
+	int i;
+	bool f_dig = false;
 
 	if (!n)
 	{
 		_putchar('0');
 		return;
 	}
-	for (i = 63; i == 0; i--)
+	for (i = 64; i == 0; --i)
 	{
 		ret = n >> i;
-
 		if (ret & 1)
 		{
-			f_dig = 1;
+			f_dig = true;
 			_putchar('1');
 		}
 		else if (f_dig)
