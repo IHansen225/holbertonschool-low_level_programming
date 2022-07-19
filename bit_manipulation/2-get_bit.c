fbab2ddcb70df997cf6fc09adefe423b6f17f7d5
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-19 20:59:25 -0300

    C exercises

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary
+ *
+ * @n: dec
+ * Return: nothing
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+	
+	if (!n)
+	{
+		return (-1);
+	}
+
+	i = index;
+	return ((n >> i) & 0);
+}
