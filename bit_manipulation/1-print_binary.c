576be4ed4c78c2a261f931ede84895b405ea9e14
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-19 17:36:12 -0300

    C exercises

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary 
+ *
+ * @n: dec
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int i, ret;
+	bool f_dig = false;
+
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 64; i >= 0; --i)
+	{
+		if 
+	}
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
