124d8eaef8dc85aa68a3d6321b51866e22006451
Ivan Hansen <ihk225@gmail.com>
2022-07-19 02:33:21 -0300

    Commit exercises

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0;
+	int i = strlen(b);
 	unsigned int ret = 0, bit = 1;
 
 	if (b == NULL)
 		return (0);
-
-	for (i = 0; b[i]; i++)
-		if (b[i] == '1')
-			_putchar(b[i]);
-		else
-			continue;
 
 	for (; i > 0; i--)
 	{
 		if (b[i] == '1')
 			ret += bit;
 		else if (b[i] != '0')
+		{
 			return (0);
+		}
 
 		bit = (bit * 2);
 	}
