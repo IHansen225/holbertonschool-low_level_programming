31d89e033e429a12e67ff1f8a6994fb6cfa699b2
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-19 21:58:53 -0300

    C exercises

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -10,12 +10,20 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int mask = 0;
+	int i = 0;
+	
 	if ((!n) || (index > 63))
 	{
 		return (-1);
 	}
-
-	*n = (*n & (0 << index));
-
+	for (; i < 64; i++)
+	{
+		if (i = index)
+			continue;
+		else
+			mask = ((*n) & (1 << i));
+	}
+	*n = mask;
 	return (1);
 }
