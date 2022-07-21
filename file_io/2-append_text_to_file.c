f2c64ecce94df0d1ac8249ab834d6403d5fb2e5a
Ivan Hansen <ihk225@gmail.com>
2022-07-20 22:50:52 -0300

    Commit from 20/7

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 	{
-		return (1);
+		return (-1);
 	}
 	ofile = open(filename, O_RDWR | O_APPEND);
 	if (ofile == -1)
