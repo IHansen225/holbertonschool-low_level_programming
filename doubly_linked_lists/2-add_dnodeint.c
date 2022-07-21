308b9016a88de86e09f79461ead8613cf3af9161
Ivan Hansen <ihk225@gmail.com>
2022-07-21 17:00:09 -0300

    Commit 21/7

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - prints list length
+ *
+ * @head: list header
+ * @n: number to add
+ * Return: 0
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newelement;
+
+	newelement = malloc(sizeof(dlistint_t));
+	if (newelement == NULL)
+		return (NULL);
+	newelement->next = *head;
+	newelement->prev = NULL;
+	newelement->n = n;
+	*head = newelement;
+
+	return (*head);
+}
