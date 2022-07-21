ee377dff0aa6ac41b953795fbc07d8e8fd4315cb
Ivan Hansen <ihk225@gmail.com>
2022-07-21 17:09:01 -0300

    Commit 21/7

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,10 +13,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *newelement;
 
 	newelement = malloc(sizeof(dlistint_t));
-	if (newelement == NULL)
+	if ( == NULL)
 		return (NULL);
 	newelement->next = *head;
 	newelement->prev = NULL;
+	newelement->next->prev = newelement;
 	newelement->n = n;
 	*head = newelement;
 
