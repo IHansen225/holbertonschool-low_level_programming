351a45a0ed77a59df857a90b8a2e5b788015ec96
Ivan Hansen <ihk225@gmail.com>
2022-07-21 13:22:15 -0300

    Commit 21/7

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -12,18 +12,11 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *p = h;
 	int i = 0;
 
-	for (; p != NULL; p = p->next)
+	for (; p != NULL; p = p->n)
 	{
-		if (p->str != NULL)
-		{
-			printf("[%u] %s", p->len, p->str);
-		}
-		else
-		{
-			printf("[0] (nil)");
-		}
-		i++;
+		printf("%d ", p->value);
 		putchar('\n');
+		i++;
 	}
 
 	return (i);
