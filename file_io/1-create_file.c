c3c418bb233d2bb4627a178c9654bffb1071c2f4
Ivan Hansen <ihk225@gmail.com>
2022-07-20 22:48:12 -0300

    Commit from 20/7

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,6 +22,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (!text_content)
+	{
+		return (1);
+	}
 	wfile = write(ofile, text_content, strlen(text_content));
 	close(ofile);
 	if (wfile == -1)
