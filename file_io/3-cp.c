bffa1a7343ee70aa930b4a07a170ee3ef0634877
Ivan Hansen <ihk225@gmail.com>
2022-07-20 23:26:39 -0300

    Commit from 20/7

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,18 +7,30 @@
 
 int main(int argc, char *argv[])
 {
-	int ofile_from;
+	int ofile_from, ofile_to, cfile;
 
 	if (argc < 3)
 	{
 		fprintf(stderr, "Usage: cp file_from file_to");
-		exit (97);
+		exit(97);
 	}
 	ofile_from = open(argv[1], O_RDONLY);
 	if (ofile_from == -1)
 	{
 		fprintf(stderr, "Error: Can't read from %s", argv[1]);
-		exit (98);
+		exit(98);
+	}
+	ofile_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (ofile_to == -1)
+	{
+		cfile = close(ofile_from);
+		if (cfile == -1)
+		{
+			fprintf(stderr, "Error: Can't close fd %d", ofile_from);
+			exit(100);
+		}
+		fprintf(stderr, "Error: Can't write to %s", argv[1]);
+		exit(99);
 	}
 
 	return (0);
