dec7d3fbd4567330b768ae1a7b4910deaf535990
Ivan Hansen <ihk225@gmail.com>
2022-07-21 16:56:50 -0300

    Commit 21/7

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - prints list length
+ *
+ * @h: list header
+ * Return: 0
+ */
+
+size_t dlistint_len(const dlistint_t *h);
+{
+	const dlistint_t *p = h;
+	int i = 0;
+
+	for (; p != NULL; p = p->next)
+	{
+		i++;
+	}
+
+	return (i);
+}
