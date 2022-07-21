23b32094304bcb2a0b2ce2cfaaaf342745325ce2
Ivan Hansen <ihk225@gmail.com>
2022-07-20 23:15:05 -0300

    Commit from 20/7

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,14 +7,18 @@
 
 int main(int argc, char *argv[])
 {
-	/*
-	 * int i, ofile, rfile, wfile;
-	*/
-	(void)argv;
+	int ofile_from;
+
 	if (argc < 3)
 	{
 		printf("Usage: cp file_from file_to");
 		exit (97);
+	}
+	ofile_from = open(argv[1], O_RDONLY);
+	if (ofile_from == -1)
+	{
+		printf("Error: Can't read from %s", argv[1]);
+		exit (98);
 	}
 
 	return (0);
