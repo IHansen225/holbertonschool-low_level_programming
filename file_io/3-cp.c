cccf501c6f719322c418725a8ac28764e46ca37c
Ivan Hansen <ihk225@gmail.com>
2022-07-20 22:56:58 -0300

    Commit from 20/7

---
+++ file_io/3-cp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+void main(int argc, char *argv[])
+{
+	int i, ofile, rfile, wfile;
+
+	if (argc < 3)
+	{
+		printf("Usage: cp file_from file_to");
+		exit (97);
+	}
+}
