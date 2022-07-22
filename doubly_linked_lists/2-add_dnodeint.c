1ddcc1e3cd4b9c8799e6edc852944bb38df7d191
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-22 18:23:40 -0300

    C exercises

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *newelement, *previous;
+	dlistint_t *newelement;
 
 	newelement = malloc(sizeof(dlistint_t));
 	if (newelement == NULL)
