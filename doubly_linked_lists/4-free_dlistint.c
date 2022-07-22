ccc451dbe94ac7e8e7bac38cc5f527bac1fb9180
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-22 17:35:08 -0300

    C exercises

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - adds node at end of list
+ *
+ * @head: list header
+ * Return: 0
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *p_elem, *elem;
+
+	elem = head;
+	while (elem)
+	{
+		p_elem = elem;
+		elem = elem->next;
+		free(p_elem);
+	}
+}
