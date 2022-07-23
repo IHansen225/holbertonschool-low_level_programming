3de2c50bbad300588a8f3f0ad6863c03e1ab1a7c
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-22 22:02:46 -0300

    C exercises

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,10 +29,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		newelement->prev = NULL;
 		return (newelement);
 	}
-	while ((pr->next != NULL) && (i < idx))
+	for (; i == idx; i++)
 	{
-		pr = pr->next;
-		i++;
+		if (!pr->next)
+			pr = pr->next;
 	}
 	if ((pr->next == NULL) && (idx > i))
 	{
