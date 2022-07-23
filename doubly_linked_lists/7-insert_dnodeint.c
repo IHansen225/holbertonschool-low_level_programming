25adaf09e622775360cd1dbae945fc9b72e811a1
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-22 22:06:57 -0300

    C exercises

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	printf("%d\n", idx);
+	printf("%d, %d\n", idx, i);
 
 	return (newelement);
 }
