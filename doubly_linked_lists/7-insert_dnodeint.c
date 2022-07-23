cc15c453e1a10ce9ba3f467decaf4c9a41c57b8a
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-22 21:29:11 -0300

    C exercises

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newelement, *pr;
-	unsigned int i;
+	unsigned int i = 0;
 
 	pr = *h;
 	newelement = malloc(sizeof(dlistint_t));
