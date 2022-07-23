c8c4934e7b134fdf9a784def91631161b13434f2
Ivan Hansen <ihansen@pop-os.localdomain>
2022-07-22 23:06:15 -0300

    C exercises

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function
+ *
+ * @h: list header
+ * @index: index
+ * @n: number to add
+ * Return: 0
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *pr, *aux;
+	unsigned int i = 0;
+
+	pr = *head;
+	if (*h == NULL)
+	{
+		return (1);
+	}
+	while ((pr->next != NULL) && (i + 1 != index))
+	{
+		pr = pr->next;
+		i++;
+	}
+	aux = pr->next->next;
+	free(pr->next);
+	pr->next = aux;
+	pr->next->prev = pr;
+
+	return (1);
+}
