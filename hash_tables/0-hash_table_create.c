0e1b599300f83af392f8aad257d8186ef9aa30c3
Ivan Hansen <ihk225@gmail.com>
2022-08-19 21:55:39 -0300

    Commit exercise 1

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a new hash table
+ * 
+ * @size: size of the hash table
+ * Return: a new hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	hash_node_t **node_arr;
+
+	ht = malloc(hash_table_t);
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	node_arr = malloc(size);
+	if (!node_arr)
+	{
+		free(ht);
+		return (NULL);
+	}
+	ht->array = node_arr;
+	return (ht);
+}
