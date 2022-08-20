14d4e1879b4e1630ff381a046809d977c5047e76
Ivan Hansen <ihk225@gmail.com>
2022-08-19 22:14:34 -0300

    i variable type change

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	hash_node_t **node_arr;
-	int i;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
