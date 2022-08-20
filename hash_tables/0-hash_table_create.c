b7cc78619838327a6d452ae78a6ba8b28be73d0b
Ivan Hansen <ihk225@gmail.com>
2022-08-19 22:13:46 -0300

    Ex1 corrections

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	hash_node_t **node_arr;
+	int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
@@ -21,6 +22,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		free(ht);
 		return (NULL);
+	}
+	for (i = 0; i <= ((size/(sizeof(hash_node_t))) - 1); i++)
+	{
+		node_arr[i] = malloc(sizeof(hash_node_t));
+		if (!node_arr[i])
+		{
+			for (; i >= 0; i--)
+				free(node_arr[i]);
+			free(ht);
+			return (NULL);
+		}
 	}
 	ht->array = node_arr;
 	return (ht);
