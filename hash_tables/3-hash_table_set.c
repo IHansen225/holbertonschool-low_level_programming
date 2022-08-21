d178e14c5674ff8ea127e1db10ac55983c5a857f
Ivan Hansen <ihk225@gmail.com>
2022-08-21 00:37:21 -0300

    .

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *aux_node;
 	unsigned long int index;
 
-	if(!ht)
+	if (!ht)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	if (!(ht->array)[index])
