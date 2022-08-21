ece54d333f408f09aa46babee4e217e349077820
Ivan Hansen <ihk225@gmail.com>
2022-08-20 22:39:50 -0300

    First attempt on indexing

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - return index of a key in the hash table
+ *
+ * @size: size of the hash table
+ * @key: key to search
+ * Return: a new hash table index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash, index;
+
+	hash = hash_djb2(key);
+	while (hash > 0)
+	{
+		if (((hash % 10) + index) < size)
+		{
+			index += (hash % 10);
+			hash = (int)(hash / 10);
+		}
+	}
+	return (index);
+}
