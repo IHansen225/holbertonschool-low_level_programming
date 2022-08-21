f6c86b5ae6dfc6771529c45b89613bca82df60d5
Ivan Hansen <ihk225@gmail.com>
2022-08-20 23:07:09 -0300

    .

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -10,16 +10,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash, index = 0;
-
-	hash = hash_djb2(key);
-	while (hash > 0)
-	{
-		if (((hash % 10) + index) < size)
-		{
-			index += (hash % 10);
-			hash = (int)(hash / 10);
-		}
-	}
-	return (index);
+	return (hash_djb2(key) % size);
 }
