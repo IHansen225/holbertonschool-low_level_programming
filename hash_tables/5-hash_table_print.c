6228cb10a570ac4a02c780b2f21ebf41b078cf63
Ivan Hansen <ihk225@gmail.com>
2022-08-21 02:22:22 -0300

    nf

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -30,6 +30,7 @@ void hash_table_print(const hash_table_t *ht)
 					printf("'%s: '", ((ht->array)[index])->key);
 					printf("'%s'", ((ht->array)[index])->value);
 					aux_node = aux_node->next;
+					nf = 1;
 				}
 			}
 		}
