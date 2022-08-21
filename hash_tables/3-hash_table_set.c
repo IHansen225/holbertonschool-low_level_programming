cd2d59fbf4deb40a16a1ca0238fa36be43799755
Ivan Hansen <ihk225@gmail.com>
2022-08-21 00:35:48 -0300

    .

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node = malloc(sizeof(hash_node_t));
 		if (!new_node)
-			return (1);
+			return (0);
 		new_node->key = strdup(key);
 		new_node->value = strdup(value);
 		new_node->next = NULL;
@@ -39,5 +39,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		(ht->array)[index] = new_node;
 	}
 
-	return (0);
+	return (1);
 }
