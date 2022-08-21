713c01ad4b9e777c2ee01f7d6ac29856176833ac
Ivan Hansen <ihk225@gmail.com>
2022-08-21 19:03:09 -0300

    .

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -15,24 +15,5 @@ void hash_table_delete(hash_table_t *ht)
 	if (!ht)
 	{}
 	else
-	{
-		for (index = 0; index < ht->size; index++)
-		{
-			if ((ht->array)[index])
-			{
-				aux_node = ht->array[index];
-				while (aux_node)
-				{
-					if (aux_node->key)
-						free(aux_node->key);
-					if (aux_node->value)
-						free(aux_node->value);
-					aux_node = aux_node->next;
-				}
-				free(aux_node);
-			}
-			free((ht->array)[index]);
-		}
 		free(ht);
-	}
 }
