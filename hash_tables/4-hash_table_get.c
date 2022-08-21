267a654988d8ced79f370fe7ff0e381288b77f13
Ivan Hansen <ihk225@gmail.com>
2022-08-21 01:25:44 -0300

    minor changes

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,19 +13,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *aux_node;
 
+	if (!ht)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (!(ht->array)[index])
 		return (NULL);
-	else
+	aux_node = (ht->array)[index];
+	while (aux_node)
 	{
-		aux_node = (ht->array)[index];
-		while (aux_node)
-		{
-			if (strcmp(aux_node->key, key) == 0)
-				return (aux_node->value);
-			else
-				aux_node = aux_node->next;
-		}
+		if (strcmp(aux_node->key, key) == 0)
+			return (aux_node->value);
+		aux_node = aux_node->next;
 	}
 	return (NULL);
 }
