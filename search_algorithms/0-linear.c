b39b044a51b703bd9fa48792deedf96add7e3cc4
Ivan <ihk225@gmail.com>
2022-08-29 18:32:49 -0300

    First commit

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear search algorithm
+ *
+ * @array: array to search for
+ * @size: number of elements
+ * @value: value to search
+ * Return: index
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
